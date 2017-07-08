f981a5ff9f8f7c3feab009984e10202b1d9f8177
Patrick Martin <pcmart03@gmail.com>
2017-07-08 00:04:55 -0400

    creates base class for weather formatter

--- weather/weather_format/formatter.py
+++ weather/weather_format/formatter.py
@@ -3,7 +3,7 @@
 from texttable import Texttable
 
 
-class weather_formatter(object):
+class WeatherFormatter(object):
     """A base class for formatting forecasts
 
     Arguments: forecast_type = current, hourly, or daily
@@ -17,5 +17,24 @@ class weather_formatter(object):
         """Overide in subclasses"""
         return timestamp
 
+    def get_percent(self, ratio):
+        return "{0:.0f}%".format(ratio * 100)
+
     def drawforecast(self):
         header = [self.type, "Temp", "Summary", "Chance Perciptation"]
+        row = []
+        table = Texttable()
+
+        table.set_deco(texttable.HEADER)
+        table.set_cols_align(['l', 'c', 'l', 'c'])
+        table.add_row(header)
+
+        for data_point in self.data:
+            table.add_row([
+                self.format_time_period(data_point["time"]),
+                data_point["temperature"],
+                data_point["summary"],
+                self.get_percent(data_point["precipProbability"])
+                ])
+
+        return table
